e4ef07c3615bff3015686ea43ddb330e8916e499
Kitzoon <68196838+Kitzoon@users.noreply.github.com>
2022-12-10 11:39:39 -0500

    Update 6407649031.lua

--- Games/6407649031.lua
+++ Games/6407649031.lua
@@ -766,6 +766,26 @@ infoSec:CreateButton("Join us on discord!", function()
     end
 end)
 
+-- Misc
+local miscSec = infoTab:CreateSection("Miscellaneous")
+
+local server = miscSec:CreateButton("Serverhop", function()
+    -- credits to: inf yield for there serverhop
+    local serverList = {}
+    
+    for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+    	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+    		serverList[#serverList + 1] = v.id
+    	end
+    end
+    
+    if #serverList > 0 then
+    	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)])
+    else
+        error("No servers found")
+    end
+end)
+
 game:GetService("UserInputService").InputBegan:Connect(function(input)
     if input.UserInputType == Enum.UserInputType.MouseButton2 and mouseDown == false then
         mouseDown = true
