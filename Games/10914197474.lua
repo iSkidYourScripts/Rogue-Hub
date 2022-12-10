5d615c5e4c99dc016c8bce29f32c8e23c9143c39
Kitzoon <68196838+Kitzoon@users.noreply.github.com>
2022-12-10 12:10:16 -0500

    Create 10914197474.lua

---
+++ Games/10914197474.lua
@@ -0,0 +1,244 @@
+if getgenv().Rogue_AlreadyLoaded ~= nil then error("Rogue Hub was already found running or you have other scripts executed!") return else getgenv().Rogue_AlreadyLoaded = 0 end
+
+if game.PlaceId ~= 10914197474 then return end
+
+local sound = Instance.new("Sound", workspace)
+sound.SoundId = "rbxassetid://1548304764"
+sound.PlayOnRemove = true
+sound.Volume = 0.5
+
+function CheckConfigFile()
+    if not isfile("/Rogue Hub/Configs/Keybind.ROGUEHUB") then return Enum.KeyCode.RightControl else return Enum.KeyCode[game:GetService("HttpService"):JSONDecode(readfile("/Rogue Hub/Configs/Keybind.ROGUEHUB"))["Key"]] or Enum.KeyCode.RightControl end
+end
+
+local Config = {
+    WindowName = "Rogue Hub | " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name or "Slap Battles",
+    Color = Color3.fromRGB(201,144,150),
+    Keybind = CheckConfigFile()
+}
+
+local localPlr = game:GetService("Players").LocalPlayer
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Bracket/main/BracketV3.lua"))()
+local window = library:CreateWindow(Config, game:GetService("CoreGui"))
+local mainTab = window:CreateTab("Killstreak test")
+
+if makefolder and isfolder and not isfolder("Rogue Hub") then
+    makefolder("Rogue Hub")
+    
+    makefolder("Rogue Hub/Configs")
+    makefolder("Rogue Hub/Data")
+end
+
+if readfile and isfile and isfile("Rogue Hub/Configs/SlapBattles_Config.ROGUEHUB") then
+    getgenv().settings = game:GetService("HttpService"):JSONDecode(readfile("Rogue Hub/Configs/SlapBattles_Config.ROGUEHUB"))
+end
+
+local function saveSettings()
+    if writefile then
+        writefile("Rogue Hub/Configs/SlapBattles_Config.ROGUEHUB", game:GetService("HttpService"):JSONEncode(getgenv().settings))
+    end
+end
+
+-- Exploits
+local exploitSec = mainTab:CreateSection("Exploits")
+
+exploitSec:CreateButton("Admin Glove Mods", function()
+    if localPlr.Character:FindFirstChild("Humanoid") then
+        localPlr.Name = "Tjleebubba"
+        localPlr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Dead)
+    end
+end)
+
+local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
+
+if teleportFunc then
+    exploitSec:CreateButton("Free Admin", function()
+        teleportFunc([[
+            if not game:IsLoaded() then
+                game.Loaded:Wait()
+            end
+            
+            local localPlr = game:GetService("Players").LocalPlayer
+            
+            repeat task.wait() until localPlr.Character ~= nil
+            
+            localPlr.PlayerGui.MainSpawnNPC3.Enabled = true
+            localPlr.PlayerGui.BnUI.TextButton.Visible = true
+            
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Rogue Hub Success",
+                Text = "Free admin has been loaded! Enjoy.",
+                Duration = 5
+            })
+            
+            task.wait(5)
+            
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Please note",
+                Text = "This was made by BluBambi, NOT Kitzoon.",
+                Duration = 10
+            })
+        ]])
+        
+        game:GetService("TeleportService"):Teleport(11556280462)
+    end)
+end
+
+-- Info
+
+local infoTab = window:CreateTab("Extra")
+local uiSec = infoTab:CreateSection("UI Settings")
+
+local uiColor = uiSec:CreateColorpicker("UI Color", function(color)
+	window:ChangeColor(color)
+end)
+
+uiColor:UpdateColor(Config.Color)
+
+local uiTog = uiSec:CreateToggle("UI Toggle", nil, function(bool)
+	window:Toggle(bool)
+end)
+
+uiTog:CreateKeybind(tostring(Config.Keybind):gsub("Enum.KeyCode.", ""), function(key)
+	if key == "Escape" or key == "Backspace" then key = "NONE" end
+	
+    if key == "NONE" then return else Config.Keybind = Enum.KeyCode[key]; writefile("/Rogue Hub/Configs/Keybind.ROGUEHUB", game:GetService("HttpService"):JSONEncode({Key = key})) end
+end)
+
+uiTog:SetState(true)
+
+local uiRainbow = uiSec:CreateToggle("Rainbow UI", nil, function(bool)
+	getgenv().rainbowUI = bool
+    
+    while getgenv().rainbowUI and task.wait() do
+        local hue = tick() % 10 / 10
+        local rainbow = Color3.fromHSV(hue, 1, 1)
+            
+        window:ChangeColor(rainbow)
+        uiColor:UpdateColor(rainbow)
+    end
+end)
+
+-- Credits
+
+local infoSec = infoTab:CreateSection("Credits")
+
+local req = http_request or request or syn.request
+
+infoSec:CreateButton("Founder of Rogue Hub: Kitzoon#7750", function()
+    setclipboard("Kitzoon#7750")
+    
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Rogue Hub Note",
+        Text = "Copied Kitzoon's discord username and tag to your clipboard.",
+        Duration = 5
+    })
+end)
+
+infoSec:CreateButton("Exploit's found by: BluBambi#9867", function()
+    setclipboard("BluBambi#9867")
+    
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Rogue Hub Note",
+        Text = "Copied BluBambi's discord username and tag to your clipboard.",
+        Duration = 5
+    })
+end)
+
+infoSec:CreateButton("Help with a lot: Kyron#6083", function()
+    setclipboard("Kyron#6083")
+    
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Rogue Hub Note",
+        Text = "Copied Kyron's discord username and tag to your clipboard.",
+        Duration = 5
+    })
+end)
+
+infoSec:CreateButton("Consider donating on PayPal!", function()
+    setclipboard("https://paypal.me/RogueHub")
+    
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Rogue Hub Note",
+        Text = "Copied our PayPal donate page to your clipboard, donate any amount to it!",
+        Duration = 5
+    })
+end)
+
+infoSec:CreateButton("Consider donating on Bitcoin!", function()
+    setclipboard("bc1qh8axzk8udu7apye7l384s5m6rt4d24rdwgkkcz")
+    
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Rogue Hub Note",
+        Text = "Copied our Bitcoin address to your clipboard, donate any amount to it!",
+        Duration = 5
+    })
+end)
+
+infoSec:CreateButton("Join us on discord!", function()
+	if req then
+        req({
+            Url = "http://127.0.0.1:6463/rpc?v=1",
+            Method = "POST",
+            
+            Headers = {
+                ["Content-Type"] = "application/json",
+                ["origin"] = "https://discord.com",
+            },
+                    
+            Body = game:GetService("HttpService"):JSONEncode(
+            {
+                ["args"] = {
+                ["code"] = "c4xWZ4G4bx",
+                },
+                
+                ["cmd"] = "INVITE_BROWSER",
+                ["nonce"] = "."
+            })
+        })
+    else
+        setclipboard("https://discord.gg/c4xWZ4G4bx")
+    
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Rogue Hub Note",
+            Text = "Copied our discord server to your clipboard.",
+            Duration = 5
+        })
+    end
+end)
+
+-- Misc
+local miscSec = infoTab:CreateSection("Miscellaneous")
+
+local server = miscSec:CreateButton("Serverhop", function()
+    -- credits to: inf yield for there serverhop
+    local serverList = {}
+    
+    for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+    	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+    		serverList[#serverList + 1] = v.id
+    	end
+    end
+    
+    if #serverList > 0 then
+    	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)])
+    else
+        error("No servers found")
+    end
+end)
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Rogue Hub Message",
+    Text = "It's almost 2023!",
+    Duration = 5
+})
+
+sound:Destroy()
+isLoaded = true
+task.wait(5)
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Rogue Hub Fact",
+    Text = "Rogue hub has over 3500+ lines of code!",
+    Duration = 10
+})
